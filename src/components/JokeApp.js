fc5109914a1ba172db9f9adf5f31b59e6230ff6c
Andrew Martins <andrew_omartins@yahoo.com>
2021-02-28 21:26:41 -0500

    I have all the code down but its not working lol

---
+++ src/components/JokeApp.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class JokeApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            joke: '',
+            isLoading: false
+        };
+    }
+    
+    
+    render() {
+        return (
+            <div>
+                <p>{this.state.isLoading ? "Loading..." : this.state.joke}</p>
+                <button onClick={this._fetchJoke}>New Joke</button>
+            </div>
+        );
+    }
+}
+
+
+
+
+_fetchJoke = () => {
+    this.setState({
+        isLoading: true,
+    }, () => {
+        const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+        fetch(url)
+            .then(response => response.json())
+            .then(jokeJson => {
+                this.setState({
+                    joke: jokeJson.value,
+                    isLoading: false
+                }, () => {
+                    console.log('New joke stored');
+                
+                });
+            })
+  
+    })
+    
+}
+
+
+export default JokeApp;
